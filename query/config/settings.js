b3de0647ef698d788dd6c1aba115223cf2514349
EC2 Default User <ec2-user@ip-10-84-140-139.eu-west-1.compute.internal>
2014-11-19 08:32:38 +0000

    Added configuration

---
+++ query/config/settings.js
@@ -0,0 +1,6 @@
+exports.settings = {
+      db_connection_url: 'mongodb://localhost:27017/lrs', 
+      source_collection: 'statements',
+      destination_collection: 'aep',
+      destination_collection_enrolment: 'matricula_per_semestres'
+}
