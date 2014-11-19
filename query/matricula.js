bf7ed9d8cdb24463c264a772c366d42a935efb8d
EC2 Default User <ec2-user@ip-10-84-140-139.eu-west-1.compute.internal>
2014-11-19 08:34:35 +0000

    Added configuration file

--- query/matricula.js
+++ query/matricula.js
@@ -1,5 +1,6 @@
 var mongojs = require('mongojs');
-var db = mongojs('mongodb://54.78.58.39:27017/lrs', ['importduran', 'matricula_per_semestres']);
+var config = require('./config/settings').settings;
+var db = mongojs(config.db_connection_url, [config.source_collection, config.destination_collection_enrolment]);
 
 var matricula = {};
 
@@ -12,18 +13,17 @@ matricula.execute = function() {
         return values.length;
     };
 
-    db.importduran.mapReduce(
-        mapper,
-        reducer, {
-            query: { "verb.id": "http://la.uoc.edu/verb/enrolment" },
-            out: "matricula_per_semestres"
-        }
-    );
-
-    db.matricula_per_semestres.find(function (err, docs) {
-        if (err) console.log(err);
-        console.log("\n", docs);
-    });
+    eval('db.'+config.source_collection+'.mapReduce('+
+        'mapper,'+
+        'reducer, {'+
+            'query: { "verb.id": "http://la.uoc.edu/verb/enrolment" },'+
+            'out: "'+config.destination_collection_enrolment+'"'+
+        '}'+
+    ');');
+    eval('db.'+config.destination_collection_enrolment+'.find(function (err, docs) {'+
+        'if (err) console.log(err);'+
+        'console.log("\\\n", docs);'+
+    '});');
 };
 
 matricula.query = function() {
