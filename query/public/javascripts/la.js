51c9b332a12472a092f4fa32a0acbff3440a08c8
Antoni Bertran <antoni@tresipunt.com>
2014-12-02 09:26:41 +0100

    Interface to allow request

---
+++ query/public/javascripts/la.js
@@ -0,0 +1,41 @@
+$( document ).ready(function() {
+
+  $('#send').click(function(){
+    launchQueryLA();
+  });
+  $('#logout').click(function(){
+    document.location.href='/logout';
+  });
+  $('#fromNext').click(function(){
+    $('#from').val(parseInt($('#from').val(),10)+parseInt($('#limit').val(),10));
+    launchQueryLA();
+  });
+  $('#fromPrevious').click(function(){
+    var value = parseInt($('#from').val(),10)+parseInt($('#limit').val(),10);
+    if (value<0) {
+      value = 0;
+    }
+    $('#from').val(value);
+    launchQueryLA();
+  });
+  launchQueryLA= function() {
+    $('#result').html("Loading ....");
+      $('#send').prop('disabled', 'disabled');
+      var data = {
+        query: $('#query').val(),
+        from: $('#from').val(),
+        limit: $('#limit').val()
+      };
+      $.post( "/dashboard", data, function(ret) {
+        $('#result').html(ret.content);
+        //hljs.highlightBlock($('#result').get(0));
+
+      })
+      .fail(function() {
+          alert("Error");
+      })
+      .always(function() {
+          $('#send').prop('disabled', '');
+      });    
+  }
+});
---
+++ query/routes/index.js
@@ -0,0 +1,118 @@
+module.exports = function (router, passport) {
+
+    var helper = require('../lib/helpers/helper.js');
+    router.get('/', helper.isAuthenticated, function(req, res) {
+      res.redirect('/dashboard');
+    });
+
+    router.get('/login', function (req, res) {
+
+
+        var e_messages = req.flash();
+        var errorMessage = "";
+        var infoMessage = "";
+        if (typeof e_messages !== 'undefined') {
+            if (typeof e_messages.error !== 'undefined' && e_messages.error.length > 0)
+                errorMessage = e_messages.error[e_messages.error.length - 1];
+            if (typeof e_messages.info !== 'undefined' && e_messages.info.length > 0)
+                infoMessage = e_messages.info[e_messages.info.length - 1];
+        }
+        if (req.user) {
+            res.redirect('/dashboard');
+        } else {
+            res.render('index', {
+                "message": errorMessage, "messageInfo": infoMessage, title: 'UOC Learning Analytics'
+            });
+        }
+    });
+
+
+    router.get('/logout', function (req, res) {
+        req.flash('info', 'The user is logout');
+        req.logout();
+        res.redirect('/login');
+    });    
+
+    // Render the dashboard page.
+    router.get('/dashboard', helper.isAuthenticated, function (req, res) {
+      res.render('dashboard', {title: 'Dashboard', user: req.user});
+    });
+
+    // Render the dashboard page.
+    router.post('/dashboard', helper.isAuthenticated, function (req, res) {
+      var hljs = require('highlight.js');
+      var hostArray = req.get('host').split(":");
+      if (hostArray.length==1) {
+        hostArray[1] = 80;
+      } else {
+        hostArray[1] = parseInt(hostArray[1], 10);
+      }
+      var from = parseInt((req.body.from)?req.body.from:0,10);
+      var limit = parseInt((req.body.limit)?req.body.limit:50,10);
+      var query = (req.body.query && req.body.query.length>0) ?req.body.query:'/lrs/statements';
+      var url = req.protocol + '://' + hostArray[0] + ":" + (hostArray[1]+1) + query+'?&limit='+limit+'&skip='+from;
+      //var url = req.protocol + '://' + hostArray[0] + ":" + (hostArray[1]+1) + '/lrs/statements';
+
+      var Client = require('node-rest-client').Client;
+
+      var client = new Client();
+
+        // set content-type header and data as json in args parameter
+        client.get(url, function (data, response) {
+          code = hljs.highlight('javascript', data);
+          var result = {
+            content: code.value
+          };
+          res.status(200).json(result);
+        }).on('error', function (err) {
+            if (err.code == 'ECONNREFUSED') {
+                console.warn('Can\'t connect to ' + url + ' review the configuration', err);
+            } else {
+                console.warn('Error connection to ' + url, err);
+            }
+            res.status(500).json(err);
+        });
+      
+    });
+
+
+    // =====================================
+    // SIGNUP ==============================
+    // =====================================
+    // show the signup form
+    router.get('/signup', function (req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.render('signup', { message: req.flash('signupMessage'), title: 'UOC Learning Analytics - Register' });
+    });
+
+    // =====================================
+    // PROFILE SECTION =====================
+    // =====================================
+    // we will want this protected so you have to be logged in to visit
+    // we will use route middleware to verify this (the isLoggedIn function)
+
+    router.get('/profile', helper.isAuthenticated, function (req, res) {
+        res.render('profile', {
+            user: req.user // get the user out of session and pass to template
+        });
+    });
+
+    router.post('/login', passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
+      function(req, res) {
+        res.redirect('/');
+      });
+
+    router.post('/signup', passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login',
+        failureFlash: true
+    }));
+
+    router.get('/error_not_authenticated',
+        function (req, res) {
+            res.render('error', { title: 'Error', message: 'User is not authenticated'});
+        }
+    );
+    
+};
