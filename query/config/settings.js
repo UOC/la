d67083899a0d11ea2da2460bba0af74e2815e363
Xavier Duran <xdurana@uoc.edu>
2014-12-18 16:38:34 +0100

    notes per estudis

--- query/config/settings.js
+++ query/config/settings.js
@@ -11,6 +11,7 @@ var settings = {
     destination_collection_aep: 'aep',
     destination_collection_grades: 'notes_per_semestres',
     destination_collection_grades_by_subject_and_semester: 'notes_per_assignatures_i_semestres',
+    destination_collection_grades_by_study_and_semester: 'notes_per_estudis_i_semestres',
     destination_collection_enrolment: 'matricula_per_semestres',
     destination_collection_enrolment_by_user: 'matricula_per_usuaris',
     destination_collection_enrolment_by_user_and_semester: 'matricula_per_usuaris_i_semestres',
