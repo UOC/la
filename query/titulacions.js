fd41c2cd6ad9a072185016cf1bca80f156f57271
Xavier Duran <xdurana@uoc.edu>
2014-12-18 16:43:20 +0100

    notes per estudis

--- query/titulacions.js
+++ query/titulacions.js
@@ -84,4 +84,142 @@ titulacions.notes = function(callback) {
     });
 }
 
+titulacions.notes_estudis = function(callback) {
+
+    db.statements.mapReduce(
+        function () {
+
+            var af = this.object.definition.extensions["edu:uoc:la:subject"].evaluation.af;
+            var result = {
+                aprovats: 0,
+                total: 0,
+                distribucio: {
+                    A: 0,
+                    B: 0,
+                    Cm: 0,
+                    Cn: 0,
+                    D: 0,
+                    N: 0,
+                    undefined: 0
+                }
+            };
+
+            result.distribucio[af]++;
+            result.total = 1;
+
+            if (af == 'A' || af == 'B' || af == 'Cm' || af == 'Cn' ) {
+                result.aprovats = result.aprovats + 1;
+            }
+
+            emit({
+                code: this.object.definition.extensions["edu:uoc:la:study"].code,
+                description: this.object.definition.extensions["edu:uoc:la:study"].description,
+                semester: this.object.definition.extensions["edu:uoc:la:semester"].code
+            }, result);
+        },
+        function(key, values) {
+            var result = {
+                aprovats: 0,
+                total: 0,
+                distribucio: {
+                    A: 0,
+                    B: 0,
+                    Cm: 0,
+                    Cn: 0,
+                    D: 0,
+                    N: 0,
+                    undefined: 0
+                }
+            };
+            values.forEach(function(value) {
+                result.aprovats += value.aprovats;
+                result.total += value.total;
+                result.distribucio.A += value.distribucio.A;
+                result.distribucio.B += value.distribucio.B;
+                result.distribucio.Cm += value.distribucio.Cm;
+                result.distribucio.Cn += value.distribucio.Cn;
+                result.distribucio.D += value.distribucio.D;
+                result.distribucio.N += value.distribucio.N;
+                result.distribucio.undefined += value.distribucio.undefined;
+            });
+            return result;
+        },
+        {
+            query: {
+                "verb.id": "http://la.uoc.edu/verb/performance"
+            },
+            out: "notes_per_estudis_i_semestres"
+        }
+    );
+}
+
+titulacions.notes_assignatures = function(callback) {
+    
+    db.statements.mapReduce(
+        function () {
+
+            var af = this.object.definition.extensions["edu:uoc:la:subject"].evaluation.af;
+            var result = {
+                aprovats: 0,
+                total: 0,
+                distribucio: {
+                    A: 0,
+                    B: 0,
+                    Cm: 0,
+                    Cn: 0,
+                    D: 0,
+                    N: 0,
+                    undefined: 0
+                }
+            };
+
+            result.distribucio[af]++;
+            result.total = 1;
+
+            if (af == 'A' || af == 'B' || af == 'Cm' || af == 'Cn' ) {
+                result.aprovats = result.aprovats + 1;
+            }
+
+            emit({
+                code: this.object.definition.extensions["edu:uoc:la:subject"].code,
+                description: this.object.definition.extensions["edu:uoc:la:subject"].description,
+                semester: this.object.definition.extensions["edu:uoc:la:semester"].code
+            }, result);
+        },
+        function(key, values) {
+            var result = {
+                aprovats: 0,
+                total: 0,
+                distribucio: {
+                    A: 0,
+                    B: 0,
+                    Cm: 0,
+                    Cn: 0,
+                    D: 0,
+                    N: 0,
+                    undefined: 0
+                }
+            };
+            values.forEach(function(value) {
+                result.aprovats += value.aprovats;
+                result.total += value.total;
+                result.distribucio.A += value.distribucio.A;
+                result.distribucio.B += value.distribucio.B;
+                result.distribucio.Cm += value.distribucio.Cm;
+                result.distribucio.Cn += value.distribucio.Cn;
+                result.distribucio.D += value.distribucio.D;
+                result.distribucio.N += value.distribucio.N;
+                result.distribucio.undefined += value.distribucio.undefined;
+            });
+            return result;
+        },
+        {
+            query: {
+                "verb.id": "http://la.uoc.edu/verb/performance"
+            },
+            out: "notes_per_estudis_i_semestres"
+        }
+    );
+}
+
 module.exports = titulacions;
